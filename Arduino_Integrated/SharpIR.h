24251a99843909d11dc8febdcdb006297b36414e
djtfoo <djt.foo@gmail.com>
2021-02-19 13:24:55 +0800

    Reduce num samples for IR readings (untested)

--- Arduino_Integrated/SharpIR.h
+++ Arduino_Integrated/SharpIR.h
@@ -10,16 +10,16 @@
 //#define MEDIAN_FILTER
 #define MEDOFMEDIANS_FILTER
 
-#define NUM_SAMPLES 25  // for median filter: read odd no. times
-#define HALF_NUM_SAMPLES 12 // index of median
+#define NUM_SAMPLES 15  // for median filter: read odd no. times
+#define HALF_NUM_SAMPLES 7 // index of median
 
 // physical sensor offsets
-#define D1_OFFSET 0 // 3.6
-#define D2_OFFSET 0 // 3.2
-#define D3_OFFSET 0 // 6.2
-#define S1_OFFSET 0 // 5.4
-#define S2_OFFSET 0
-#define LR_OFFSET 0
+#define D1_OFFSET 0 // 5.0
+#define D2_OFFSET 0 // 4.9
+#define D3_OFFSET 0 // 5.0
+#define S1_OFFSET 0 // TBC
+#define S2_OFFSET 0 // TBC
+#define LR_OFFSET 0 // 9.9
 
 #include <Arduino.h>
 
